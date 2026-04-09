fa998e1da13628538e87518b1a994ebdc76366fa
Dusklove28 <1774799513@qq.com>
2026-04-09 20:59:54 +0800

    第三次实验覆盖

--- task/experiment_config.py
+++ task/experiment_config.py
@@ -1,5 +1,3 @@
-from plot_final_battle import model_path
-
 EXPERIMENT_FUNCTIONS = [1,11]
 EXPERIMENT_RUNTIMES = 10
 EXPERIMENT_SEPARATE_TRAINS = [True]
@@ -12,15 +10,6 @@ EXPERIMENT_MAX_FE = int(1e4)
 EXPERIMENT_N_PART = 100
 EXPERIMENT_LR_CRITIC = 1e-4
 EXPERIMENT_LR_ACTOR = 1e-6
-EXPERIMENT_Model_Path = model_path
-
-class update():
-    def __init__(self, model_path=None):
-        self.model_path = model_path
-        # 关键：修改全局变量 EXPERIMENT_Model_Path
-        if model_path:
-            global EXPERIMENT_Model_Path
-            EXPERIMENT_Model_Path = model_path
 
 
 def get_primary_experiment_signature():
@@ -30,8 +19,6 @@ def get_primary_experiment_signature():
         raise ValueError("Standalone evaluation expects exactly one configured group.")
     if len(EXPERIMENT_SEPARATE_TRAINS) != 1:
         raise ValueError("Standalone evaluation expects exactly one separate_train setting.")
-    if len(EXPERIMENT_Model_Path) != 1:
-        raise ValueError("Model Path not exist")
 
     return {
         'dim': EXPERIMENT_DIMS[0],
